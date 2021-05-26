c792abc49c0709e9e82f599b099688bcab82f4aa
Guilherme Boves <ghboves@gmail.com>
2021-05-25 22:47:54 -0300

    Part-5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,22 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component'; // <-- NgModel lives here
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component'; // <-- NgModel lives here
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -13,18 +13,11 @@ export class HeroesComponent implements OnInit {
   
   heroes: Hero[] = [];
 
-  selectedHero?: Hero;
-
   constructor(private heroService: HeroService, private messageService: MessageService) {}
   
   ngOnInit() {
     this.getHeroes();
   }
-
-  onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-  this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-}
 
 getHeroes(): void {
   this.heroService.getHeroes()
