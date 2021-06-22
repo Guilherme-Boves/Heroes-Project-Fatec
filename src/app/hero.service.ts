07c5c4915efda197fea931fec66ba44b2b4b702e
Guilherme Boves <ghboves@gmail.com>
2021-06-22 20:28:34 -0300

    Connected to Backend SpringBoot

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,7 +11,7 @@ import { catchError, map, tap } from 'rxjs/operators';
 })
 export class HeroService {
 
-  private heroesUrl = 'http://localhost:3000/heroes';  // URL to web api
+  private heroesUrl = 'http://localhost:8080/heroes';  // URL to web api
 
   private httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
